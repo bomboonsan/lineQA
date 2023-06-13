7d3a04a963c0147486530a2ec56597969973cfab
bomboonsan <bomboonsan@gmail.com>
2023-06-13 07:25:38 +0700

    fix_liff

--- components/event/Content.js
+++ components/event/Content.js
@@ -67,22 +67,28 @@ export default function Content( {pageData} ) {
     //       }
     //     }    
     //     initializeLiff();
-
-
     // }, []);
 
     // START LINE LIFF NEW
-    // useEffect(async () => {
-    //     const liff = (await import('@line/liff')).default
-    //     try {
-    //       await liff.init({ liffId });
-    //     } catch (error) {
-    //       console.error('liff init error', error.message)
-    //     }
-    //     if (!liff.isLoggedIn()) {
-    //       liff.login();
-    //     }
-    // })
+    useEffect(async () => {
+        const liff = (await import('@line/liff')).default
+        try {
+            await liff.init({ liffId: '1661407578-X6ro31ow', });
+
+
+            const profile = await liff.getProfile();
+            const { displayName, pictureUrl, email } = profile;
+            console.log('User Name:', displayName);
+            console.log('Picture URL:', pictureUrl);
+            console.log('Email:', email);
+
+        } catch (error) {
+          console.error('liff init error', error.message)
+        }
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        }
+    })
 
     return (
         <>

27c4342129cf943b3cc1e84ae0aa2db36636cac7
bomboonsan <bomboonsan@gmail.com>
2023-06-22 10:37:12 +0700

    i

--- components/event/result.js
+++ components/event/result.js
@@ -14,6 +14,8 @@ export default function Result( {pageData} ) {
   const [prefixUrl, setPrefixUrl] = useState("https://api.bomboonsan.com/");
   const [globolUser, setGlobolUser] = useRecoilState(stateUser)
 
+  const [urlSharedExternal, setUrlSharedExternal] = useState(null);
+
   // console.log(pageData)
 
   const nativeShare = ()=> {
@@ -59,8 +61,10 @@ export default function Result( {pageData} ) {
       if (index > 0) {
         setIndexResult(index)
       } else {
-        setIndexResult(1)
+        setIndexResult(0)
       }
+
+      setUrlSharedExternal(`https://boschthailand.aclick.asia/result?id=${pageData._id}&name=${encodeURIComponent(globolUser.displayName)}&point=${globolUser.point}?openExternalBrowser=1`)
 
     }
 
@@ -98,12 +102,12 @@ export default function Result( {pageData} ) {
           </section>
           <section className='p-3'>
             <div className='mt-5'>
-              <button className="btn btn-block btn-primary text-xl text-white rounded-[0px]" onClick={nativeShare}>
+              <a className="btn btn-block btn-primary text-xl text-white rounded-[0px]" href={urlSharedExternal}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share" viewBox="0 0 16 16">
                   <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
                 </svg>
                 แชร์ผลงาน
-              </button>
+              </a>
               
               <div className='mt-3'>
                 <button className="btn btn-block btn-error text-xl text-white rounded-[0px]" onClick={() => router.reload()}>
